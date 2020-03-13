2bd4bad046a32304efa98e4398b803ad3c3830f5
154852 <louisemileploix@icloud.com>
2020-03-13 20:14:23 +0000

    Prepared for server use

--- server/src/reset.ts
+++ server/src/reset.ts
@@ -14,57 +14,61 @@ const sequelize: Sequelize = new Sequelize({
     models: [SessionKey, User, Game, Review]
 });
 
-sequelize.drop().then(() => sequelize.sync({ force: true })).then(() => {
-    for (let i = 1; i <= 3; i++) {
-        User.signUpUser({
-            username: "Username" + i,
-            password: "Password" + i,
-            email: `email${i}@mail.com`
-        }).then(([user, key]) => {
-            if (i == 1) {
-                user.permissionLevel = 1;
-                user.save();
+sequelize.query("SET FOREIGN_KEY_CONSTRAINTS=0").then(() => {
+    sequelize.drop().then(() => sequelize.sync({ force: true })).then(() => {
+        for (let i = 1; i <= 3; i++) {
+            User.signUpUser({
+                username: "Username" + i,
+                password: "Password" + i,
+                email: `email${i}@mail.com`
+            }).then(([user, key]) => {
+                if (i == 1) {
+                    user.permissionLevel = 1;
+                    user.save();
+                }
+            });
+        }
+
+        const titles: string[] = [
+            "Commodo incididunt nostrud tempor nostrud labore incididunt commodo eiusmod excepteur.",
+            "Magna reprehenderit minim exercitation laboris velit officia cupidatat voluptate excepteur incididunt commodo.",
+            "Esse nisi exercitation non deserunt labore minim incididunt deserunt.",
+            "Ex ullamco nulla labore tempor voluptate occaecat.",
+            "Excepteur ipsum in consectetur mollit ex esse ad aliqua tempor elit.",
+            "Excepteur Lorem mollit Lorem sit commodo.",
+            "Ut eiusmod in veniam ea incididunt magna proident pariatur sint do tempor laborum labore.",
+            "Excepteur ipsum id occaecat veniam ea duis officia eiusmod eu reprehenderit et aliqua ut.",
+            "Et sunt magna excepteur quis aute do non aute minim consectetur cupidatat ex."
+        ];
+
+        (Game.createGameFromOptions({
+            name: "League Of Legends",
+            description: "A description",
+            imageURL: "https://wallpaperaccess.com/full/217097.jpg"
+        }) as Promise<Game>).then((game) => {
+            for (let i = 1; i <= 10; i++) {
+                Review.createReviewFromOptions({
+                    title: "Occaecat ad elit voluptate magna aliquip non voluptate aute culpa dolore anim. " + game.id + ":" + i,
+                    body: "A review, with index" + (i - 1),
+                    gameID: game.id
+                });
             }
         });
-    }
 
-    const titles: string[] = [
-        "Commodo incididunt nostrud tempor nostrud labore incididunt commodo eiusmod excepteur.",
-        "Magna reprehenderit minim exercitation laboris velit officia cupidatat voluptate excepteur incididunt commodo.",
-        "Esse nisi exercitation non deserunt labore minim incididunt deserunt.",
-        "Ex ullamco nulla labore tempor voluptate occaecat.",
-        "Excepteur ipsum in consectetur mollit ex esse ad aliqua tempor elit.",
-        "Excepteur Lorem mollit Lorem sit commodo.",
-        "Ut eiusmod in veniam ea incididunt magna proident pariatur sint do tempor laborum labore.",
-        "Excepteur ipsum id occaecat veniam ea duis officia eiusmod eu reprehenderit et aliqua ut.",
-        "Et sunt magna excepteur quis aute do non aute minim consectetur cupidatat ex."
-    ];
+        (Game.createGameFromOptions({
+            name: "Undertale",
+            description: "Another description",
+            imageURL: "https://fontmeme.com/images/undertale-font.jpg"
+        }) as Promise<Game>).then((game) => {
+            for (let i = 1; i <= 10; i++) {
+                Review.createReviewFromOptions({
+                    title: "Occaecat ad elit voluptate magna aliquip non voluptate aute culpa dolore anim. " + game.id + ":" + i,
+                    body: "A review, with index" + (i - 1),
+                    gameID: game.id
+                });
+            }
+        });
 
-    (Game.createGameFromOptions({
-        name: "League Of Legends",
-        description: "A description",
-        imageURL: "https://wallpaperaccess.com/full/217097.jpg"
-    }) as Promise<Game>).then((game) => {
-        for (let i = 1; i <= 10; i++) {
-            Review.createReviewFromOptions({
-                title: "Occaecat ad elit voluptate magna aliquip non voluptate aute culpa dolore anim. " + game.id + ":" + i,
-                body: "A review, with index" + (i - 1),
-                gameID: game.id
-            });
-        }
+        sequelize.query("SET FOREIGN_KEY_CONSTRAINTS=1");
     });
-
-    (Game.createGameFromOptions({
-        name: "Undertale",
-        description: "Another description",
-        imageURL: "https://fontmeme.com/images/undertale-font.jpg"
-    }) as Promise<Game>).then((game) => {
-        for (let i = 1; i <= 10; i++) {
-            Review.createReviewFromOptions({
-                title: "Occaecat ad elit voluptate magna aliquip non voluptate aute culpa dolore anim. " + game.id + ":" + i,
-                body: "A review, with index" + (i - 1),
-                gameID: game.id
-            });
-        }
-    });
-});
+})
